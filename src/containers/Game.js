d3e6d07f2603e880ee83631344c5a772c97baf23
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-04-04 16:01:29 -0400

    added css

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -5,17 +5,17 @@ class Game extends Component {
   render() {
     return (
       <div className="container" id="game-container">
-        <div className="item" id="avatar-holder"></div>
-        <div className="item" id="orders-holder"></div>
-        <div className="item" id="serve-holder"></div>
-        <div className="item" id="trash-holder"></div>
-        <div className="item" id="washer-holder"></div>
-        <div className="item" id="tips-holder"></div>
-        <div className="item" id="clock-holder"></div>
-        <div className="item" id="ingredients-holder"></div>
-        <div className="item" id="cookspace-holder"></div>
-        <div className="item" id="tools-holder"></div>
-        <div className="item" id="controlpanel-holder">Hi there!</div>
+        <div className="item" id="avatar-holder"><img id="game-avatar" alt="avatar" style={{width: "100px", height: "100px", "border-radius": "4px"}} src={this.props.user.avatar}/></div>
+        <div className="item" id="orders-holder"><img alt="order" src={this.props.level.recipes[0].image}/></div>
+        <div className="item" id="trash-holder">Trash can</div>
+        <div className="item" id="serve-holder"><button>Serve Button</button></div>
+        <div className="item" id="washer-holder">Dish washer</div>
+        <div className="item" id="tips-holder">Tips: $100</div>
+        <div className="item" id="clock-holder">Clock countdown: 90 seconds</div>
+        <div className="item" id="ingredients-holder">List of ingredients</div>
+        <div className="item" id="cookspace-holder">Cook space</div>
+        <div className="item" id="tools-holder">List of tools</div>
+        <div className="item" id="controlpanel-holder">User control panel</div>
       </div>
     )
   }
